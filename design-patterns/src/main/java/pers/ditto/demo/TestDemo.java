6159854fc360e35f4eaf098a77c7764169af6c5d
OrangeCH3 <hanjie1225@126.com>
2021-07-06 15:29:10 +0800

    Second commit

---
+++ design-patterns/src/main/java/pers/ditto/demo/TestDemo.java
@@ -0,0 +1,12 @@
+package pers.ditto.demo;
+
+/**
+ * @author OrangeCH3
+ * @create 2021-07-06 15:16
+ */
+public class TestDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Code to Picture!");
+    }
+}
