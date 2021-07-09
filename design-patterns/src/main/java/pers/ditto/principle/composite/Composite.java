565810c057a675237452eba473868445e6d39f8e
OrangeCH3 <hanjie1225@126.com>
2021-07-09 12:57:35 +0800

    Cont3rd-12th commit

---
+++ design-patterns/src/main/java/pers/ditto/principle/composite/Composite.java
@@ -0,0 +1,21 @@
+package pers.ditto.principle.composite;
+
+import org.junit.Test;
+
+/**
+ * @author OrangeCH3
+ * @create 2021-07-09 12:33
+ */
+
+@SuppressWarnings("all")
+public class Composite {
+
+    @Test
+    public void test() {
+
+        // 应用UML类图进行理解
+        System.out.println("类图见根文件夹");
+
+    }
+}
+
